d1cf453c81da793d999543d7c12c10e103fb20a9
Jalpesh Ahir <jalpeshahir6512@gmail.com>
2022-01-28 15:17:25 +0530

    ADD: redux for cars

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import { Header } from "./components/Header";
 import Home from "./components/Home";
-import Section from "./components/Section";
+// import Section from "./components/Section";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Home />
-      <Section />
+      {/* <Section /> */}
     </div>
   );
 }
